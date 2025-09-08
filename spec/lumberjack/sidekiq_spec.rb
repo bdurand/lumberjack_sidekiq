98b544d3aabf620e987a3afda53028b8117126b1
Brian Durand <bbdurand@gmail.com>
2025-09-08 09:43:09 -0700

    update spec

--- spec/lumberjack/sidekiq_spec.rb
+++ spec/lumberjack/sidekiq_spec.rb
@@ -3,7 +3,9 @@
 require "spec_helper"
 
 RSpec.describe Lumberjack::Sidekiq do
-  it "has a version number" do
-    expect(Lumberjack::Sidekiq::VERSION).not_to be nil
+  describe "VERSION" do
+    it "has a version number" do
+      expect(Lumberjack::Sidekiq::VERSION).not_to be nil
+    end
   end
 end
